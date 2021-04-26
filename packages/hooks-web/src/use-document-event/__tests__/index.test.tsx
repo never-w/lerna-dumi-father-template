f04b2684f20ff1d60ff606927f45cbd05860ff9a
LING_ZI_QING <onlylingstudio@gmail.com>
2021-04-26 15:51:33 +0800

    test(todo usedocumentevent tests): 准备做 useDocumentEvent 的测试用例

---
+++ packages/hooks-web/src/use-document-event/__tests__/index.test.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+
+import useDocumentEvent from '../';
+
+const Foo: React.FC<{ onClick?: any; onAnotherClick?: any; capture?: boolean }> = ({ onClick, onAnotherClick, capture }) => {
+  const [useAnother, setUseAnother] = useState(false);
+
+  useDocumentEvent('click', useAnother ? onAnotherClick : onClick, capture);
+
+  return <span role="switch" onClick={() => setUseAnother((v) => !v)} />;
+};
+
+describe('useDocumentEvent', () => {
+  test('trigger single event', () => {
+    const counter = { value: 1 };
+
+    const { baseElement } = render(<Foo onClick={() => counter.value++} />);
+
+    fireEvent.click(baseElement);
+
+    expect(counter.value).toBe(2);
+  });
+
+  test('change handler', () => {
+    const counter = { value: 1 };
+    const { baseElement, container } = render(<Foo onClick={() => {}} onAnotherClick={() => counter.value++} />);
+    fireEvent.click(screen.getByRole('switch'));
+    fireEvent.click(baseElement);
+
+    expect(counter.value).toBe(2);
+  });
+
+  test('remove on unmount', () => {
+    const counter = { value: 1 };
+    const { baseElement, unmount } = render(<Foo onClick={() => counter.value++} />);
+
+    unmount();
+
+    fireEvent.click(baseElement);
+
+    expect(counter.value).toBe(1);
+  });
+
+  test('with options', () => {
+    const counter = { value: 1 };
+    const { baseElement, unmount } = render(<Foo capture onClick={() => counter.value++} />);
+
+    unmount();
+
+    fireEvent.click(baseElement);
+
+    expect(counter.value).toBe(1);
+  });
+});
